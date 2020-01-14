a97976e236b8c31d491c32313b5cee5d73dec317
Ha Nguyen <nkei1908@yahoo.com>
2020-01-14 20:06:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,6 +12,11 @@ def map_to_negativize(array)
 end 
 
 def map_to_no_change(array)
-  
-  
+  new_array = []
+  index = 0 
+  while array[index] do 
+    new_array.push(array[index])
+    index += 1 
+  end 
+  new_array
 end 
