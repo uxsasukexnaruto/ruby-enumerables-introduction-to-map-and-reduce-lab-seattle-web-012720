1a574606487b2de075430531e0105e95491dee73
Ha Nguyen <nkei1908@yahoo.com>
2020-01-14 20:07:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,3 +20,13 @@ def map_to_no_change(array)
   end 
   new_array
 end 
+
+def map_to_double(array)
+  new_array = []
+  index = 0 
+  while array[index] do 
+    new_array.push(array[index]*2)
+    index += 1 
+  end 
+  new_array
+end 
