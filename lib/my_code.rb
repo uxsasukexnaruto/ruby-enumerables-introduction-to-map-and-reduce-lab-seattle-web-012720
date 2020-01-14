92c4c90768fcbae29d4eb00a2d82dff09b6b9bb6
Ha Nguyen <nkei1908@yahoo.com>
2020-01-14 20:05:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,11 +3,9 @@ def map_to_negativize(array)
   index = 0 
   new_array = []
   while array[index] do 
-    if array[index] > 0 
+    
       new_array.push(array[index]* -1)
-    else 
-      new_array.push(array[index])
-    end 
+    
     index += 1 
   end 
   new_array
